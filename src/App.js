e4fd70d92eacb0848bb4edcf7c23aa281cc535ba
Stephen Pal-George <stephenngeorge@gmail.com>
2019-11-19 08:31:03 +0000

    loris and ministrylib working, test data file pattern

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const App = () => (
-  <div className="App">
-
-  </div>
-)
+const App = () => {
+  return (
+    <div className="App">
+      
+    </div>
+  )
+} 
 
 export default App
