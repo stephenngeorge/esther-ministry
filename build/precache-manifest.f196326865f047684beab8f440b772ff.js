9b2e1251527572b5c298322f9cedf3f36eadb774
Stephen Pal-George <stephenngeorge@gmail.com>
2019-12-07 12:28:14 +0000

    release v1.1.3

---
+++ build/precache-manifest.f196326865f047684beab8f440b772ff.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85058aa583a055210660dc8877027b8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79083c74aa07751da6ad",
+    "url": "/static/css/2.ba88667d.chunk.css"
+  },
+  {
+    "revision": "f876d24d081003136518",
+    "url": "/static/css/main.2547a732.chunk.css"
+  },
+  {
+    "revision": "79083c74aa07751da6ad",
+    "url": "/static/js/2.42a339d9.chunk.js"
+  },
+  {
+    "revision": "f876d24d081003136518",
+    "url": "/static/js/main.34f78d89.chunk.js"
+  },
+  {
+    "revision": "8f9e6a7c651c331357b4",
+    "url": "/static/js/runtime-main.441e8631.js"
+  },
+  {
+    "revision": "a78927d6a461105200416560cff0109c",
+    "url": "/static/media/Esther_Logo_Green.a78927d6.jpg"
+  },
+  {
+    "revision": "27626726f72f4075113e78371a53f815",
+    "url": "/static/media/GenRylett_portrait.27626726.jpg"
+  },
+  {
+    "revision": "7515d0fdef87693e58f85063f2e55461",
+    "url": "/static/media/LouieThorpe_portrait.7515d0fd.jpg"
+  },
+  {
+    "revision": "7e88da89dc7b18303763da05d44bc2d2",
+    "url": "/static/media/Rowena_1.7e88da89.jpg"
+  },
+  {
+    "revision": "154ab8d0964d1145e8a0bac78254d6c7",
+    "url": "/static/media/bible_background.154ab8d0.jpg"
+  },
+  {
+    "revision": "86b21a6c543730f4883aaa90d21df921",
+    "url": "/static/media/ro_BGPortrait.86b21a6c.jpg"
+  },
+  {
+    "revision": "53a9a27daa48e87991e66b701a06e8ee",
+    "url": "/static/media/sharronCoburn_portrait.53a9a27d.jpg"
+  },
+  {
+    "revision": "8f1af8713b52c37760e2b8f700e5e268",
+    "url": "/static/media/susiePerkin_portrait.8f1af871.jpg"
+  }
+]);
